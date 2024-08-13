5c1ecc29d090b0eaefb5080b24f6da89c2c16771
Angela Gnazzo <martinpedraza1979@gmail.com>
2024-08-13 19:18:46 +0200

    new styles and clicks

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const imagesArray = [
+	{ heroName: "capitain", imageUrl: "capitain.jpg" },
+	{ heroName: "ironman", imageUrl: "ironman.jpg" },
+]
+
 const cards_array = document.querySelectorAll(".card")
+
 cards_array.forEach((card) => {
 	card.addEventListener("click", (e) => {
-		card.classList.add("animate__rotate")
-		e.target.setAttribute("data-rotate", "true")
+		if (e.target.getAttribute("data-rotate") === "false") {
+			card.classList.add("animate__rotate")
+			e.target.setAttribute("data-rotate", "true")
+		} else {
+			card.classList.remove("animate__rotate")
+			e.target.setAttribute("data-rotate", "false")
+		}
 	})
 })
